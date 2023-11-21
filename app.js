097c24a34f5b8356be15dab6cff56c34b78e73d2
Siddharth Gupta <siddharthgupta@Siddharths-MacBook-Pro-2.local>
2023-11-20 23:28:01 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./Lab5.js";
+import cors from "cors";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import "dotenv/config";
+const app = express();
+app.use(express.json());
+app.use(cors());
+Lab5(app);
+Hello(app);
+CourseRoutes(app);
+ModuleRoutes(app);
+app.listen(process.env.PORT || 4000);
