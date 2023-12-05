8b012da3ba5e38bb956a1d8fa8121bef7b86e712
Siddharth Gupta <siddharthgupta555t@gmail.com>
2023-12-05 01:04:31 -0500

    a6: able to signin after signup

--- users/routes.js
+++ users/routes.js
@@ -35,7 +35,8 @@ function UserRoutes(app) {
             res.status(400).json(
                 { message: "Username already taken" });
         }
-        req.session['currentUser'] = await dao.createUser(req.body);
+        const currentUser = await dao.createUser(req.body);
+        req.session['currentUser'] = currentUser;
         res.json(currentUser);
     };
 
