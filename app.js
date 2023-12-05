51b1c6f3b6c530f1086e227f354072962b3e7c62
Siddharth Gupta <siddharthgupta555t@gmail.com>
2023-12-04 22:00:25 -0500

    a6: initial commit

--- app.js
+++ app.js
@@ -2,16 +2,41 @@ import express from "express";
 import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./users/routes.js";
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import "dotenv/config";
 import AssignmentRoutes from "./assignments/routes.js";
+import session from "express-session";
+import "dotenv/config";
+
 const app = express();
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+}));
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 app.listen(process.env.PORT || 4000);
